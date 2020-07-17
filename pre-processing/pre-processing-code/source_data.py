74266b087195b2304b94932c2c144ff374732911
Ayush Varma <avarma6@illinois.edu>
2020-07-17 11:27:51 -0500

    Able to write xls and csv files

--- pre-processing/pre-processing-code/source_data.py
+++ pre-processing/pre-processing-code/source_data.py
@@ -11,8 +11,10 @@ def data_to_s3(frmt):
 
     source_dataset_url = 'https://fred.stlouisfed.org/graph/fredgraph'
 
+    url_end = '?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1168&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=BAMLH0A0HYM2&scale=left&cosd=2015-07-16&coed=2020-07-16&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Daily%2C%20Close&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2020-07-17&revision_date=2020-07-17&nd=1996-12-31'
+
     try:
-        response = urlopen(source_dataset_url + frmt)
+        response = urlopen(source_dataset_url + frmt + url_end)
 
     except HTTPError as e:
         raise Exception('HTTPError: ', e.code, frmt)
@@ -21,9 +23,8 @@ def data_to_s3(frmt):
         raise Exception('URLError: ', e.reason, frmt)
 
     else:
-      #  data_set_name = os.environ['DATA_SET_NAME']
-        data_set_name = 'AVtest'
-        filename = data_set_name
+        data_set_name = os.environ['DATA_SET_NAME']
+        filename = data_set_name + frmt
         file_location = 'C:/Users/Ayush Varma/Desktop/' + filename
 
         with open(file_location, 'wb') as f:
@@ -32,28 +33,27 @@ def data_to_s3(frmt):
             f.close()
 
         # variables/resources used to upload to s3
-        # s3_bucket = os.environ['S3_BUCKET']
-        # new_s3_key = data_set_name + '/dataset/'
-        # s3 = boto3.client('s3')
+        s3_bucket = os.environ['S3_BUCKET']
+        new_s3_key = data_set_name + '/dataset/'
+        s3 = boto3.client('s3')
 
         # s3.upload_file(file_location, s3_bucket, new_s3_key + filename)
 
         print('Uploaded: ' + filename)
 
         # deletes to preserve limited space in aws lamdba
-        # os.remove(file_location)
+        os.remove(file_location)
 
         # dicts to be used to add assets to the dataset revision
-        # return {'Bucket': s3_bucket, 'Key': new_s3_key + filename}
+        return {'Bucket': s3_bucket, 'Key': new_s3_key + filename}
 
 
 def source_dataset():
 
     # list of enpoints to be used to access data included with product
     data_endpoints = [
-        '.xls?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1168&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=BAMLH0A0HYM2&scale=left&cosd=2015-07-16&coed=2020-07-16&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Daily%2C%20Close&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2020-07-17&revision_date=2020-07-17&nd=1996-12-31',
-
-        '.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1168&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=BAMLH0A0HYM2&scale=left&cosd=2015-07-16&coed=2020-07-16&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Daily%2C%20Close&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2020-07-17&revision_date=2020-07-17&nd=1996-12-31'
+        '.xls',
+        '.csv'
     ]
 
     # multithreading speed up accessing data, making lambda run quicker
@@ -62,6 +62,3 @@ def source_dataset():
 
     # asset_list is returned to be used in lamdba_handler function
     return asset_list
-
-
-source_dataset()
